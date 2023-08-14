5b7fd8841c66dbee045d17e97c7d678958d50989
Ali Ebrahimi <a.ebrahimi@wallex.net>
2023-08-14 17:30:02 +0330

    add solidity node and AST

--- src/main/java/org/example/SolidityEventEmitRemover.java
+++ src/main/java/org/example/SolidityEventEmitRemover.java
@@ -8,33 +8,40 @@ import java.util.List;
 import java.util.Objects;
 
 public class SolidityEventEmitRemover extends SolidityBaseListener {
-    private ParseTreeProperty<ParseTree> modifiedTreeProperty;
+    private ParseTree modifiedTree;
 
-    public SolidityEventEmitRemover(ParseTreeProperty<ParseTree> modifiedTreeProperty) {
-        this.modifiedTreeProperty = modifiedTreeProperty;
+    private SolidityAST ast;
+
+    private ParseTreeProperty<ParseTree> modifiedTreeProperty = new ParseTreeProperty<>();
+
+    public SolidityEventEmitRemover(ParseTree tree, SolidityAST ast) {
+        this.modifiedTree = tree;
+        this.ast = ast;
     }
 
     @Override
     public void enterEventDefinition(SolidityParser.EventDefinitionContext ctx) {
+        System.out.println("Enter event definition, ctx is: " + ctx.getText());
+
         // Exclude event declarations from the modified AST
-        modifiedTreeProperty.put(ctx, null);
+        SolidityNode currentNode = new SolidityNode(ctx);
+        ast.removeNode(currentNode);
     }
 
     @Override
     public void exitEventDefinition(SolidityParser.EventDefinitionContext ctx) {
-//         modifiedTreeProperty.put(ctx, ctx);
         System.out.println("Exit event definition");
     }
 
     @Override
     public void enterEmitStatement(SolidityParser.EmitStatementContext ctx) {
         // Exclude emit statements from the modified AST
-        modifiedTreeProperty.put(ctx, null);
+        SolidityNode currentNode = new SolidityNode(ctx);
+        ast.removeNode(currentNode);
     }
 
     @Override
     public void exitEmitStatement(SolidityParser.EmitStatementContext ctx) {
-//         modifiedTreeProperty.put(ctx, ctx);
         System.out.println("Exit emit definition");
     }
 
@@ -43,13 +50,13 @@ public class SolidityEventEmitRemover extends SolidityBaseListener {
 
     }
 
-    @Override
-    public void exitFunctionDefinition(SolidityParser.FunctionDefinitionContext ctx) {
-        // Modify the function's children in the modified AST
-        List<ParseTree> modifiedChildren = removeNulls(ctx.children);
-        ctx.children = List.of(modifiedChildren.toArray(new ParseTree[0]));
-        modifiedTreeProperty.put(ctx, ctx);
-    }
+//    @Override
+//    public void exitFunctionDefinition(SolidityParser.FunctionDefinitionContext ctx) {
+//        // Modify the function's children in the modified AST
+//        List<ParseTree> modifiedChildren = removeNulls(ctx.children);
+//        ctx.children = List.of(modifiedChildren.toArray(new ParseTree[0]));
+////        modifiedTreeProperty.put(ctx, ctx);
+//    }
 
     // Helper method to remove nulls from a list of children
     private List<ParseTree> removeNulls(List<ParseTree> children) {
@@ -57,8 +64,9 @@ public class SolidityEventEmitRemover extends SolidityBaseListener {
         return children;
     }
 
-    public ParseTree getModifiedTree(ParseTree currentParseTree) {
-        return modifiedTreeProperty.get(currentParseTree);
+    public SolidityAST getModifiedTree() {
+//        return modifiedTreeProperty.get(currentParseTree);
+        return this.ast;
     }
 }
 
---
+++ src/main/java/org/example/SolidityNode.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class SolidityNode {
+    private final ParseTree node;
+    private final List<SolidityNode> children = new ArrayList<>();
+
+    public SolidityNode(ParseTree node) {
+        this.node = node;
+    }
+
+    public ParseTree getNode() {
+        return node;
+    }
+
+    public void addChild(SolidityNode child) {
+        children.add(child);
+    }
+
+    public boolean removeChild(SolidityNode child) {
+        for (SolidityNode node : children) {
+            if (Objects.equals(node.getText(), child.getText())) {
+                return children.remove(child);
+            }
+        }
+        for (SolidityNode node : children) {
+            if (node.removeChild(child)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getText() {
+        StringBuilder sb = new StringBuilder();
+
+        if (children.isEmpty()) {
+            return node.getText();
+        }
+        // Call getText recursively on children until there are no more children
+        for (SolidityNode child : children) {
+            sb.append(child.getText());
+        }
+
+        return sb.toString();
+    }
+
+    public void addChildFromParseTree(ParseTree child) {
+        SolidityNode childNode = new SolidityNode(child);
+        for (int i = 0; i < child.getChildCount(); i++) {
+            childNode.addChildFromParseTree(child.getChild(i));
+        }
+        addChild(childNode);
+    }
+}
